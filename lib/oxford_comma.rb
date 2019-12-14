244e7439299fec94709b9aae9b346b80486fe173
Khadijah Jennings <kjennings855@gmail.com>
2019-12-14 06:14:28 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,13 @@
-def oxford_comma 
- "kiwi"
+def oxford_comma(array)
+
+end 
+if array.size == 1
+  array.join
+elsif array.size == 2
+  array.join(" and ")
+else
+  last_str = array.pop
+  first_str = array.join (", ")
+  final_str = first_str +", and #{last_str}"
 end
-  
-  
+end
