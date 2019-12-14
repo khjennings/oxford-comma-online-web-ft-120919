fa2706fb16487900476d108d98f63f1e7cdd7ef6
Khadijah Jennings <kjennings855@gmail.com>
2019-12-14 06:15:07 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,11 @@
 def oxford_comma(array)
-
-end 
-if array.size == 1
+  if array.size == 1
   array.join
-elsif array.size == 2
+  elsif array.size == 2
   array.join(" and ")
-else
+  else
   last_str = array.pop
   first_str = array.join (", ")
   final_str = first_str +", and #{last_str}"
-end
+  end
 end
